d00ca6699ffa8ddba571cc7fd071f772c23406d3
Mendel <mendelbak@gmail.com>
2017-10-22 12:58:07 -0500

    HW1 files, not working. Sending to TA for consult

---
+++ Javascript/HW1.js
@@ -0,0 +1,18 @@
+var num1 = 613;
+var num2 = 770;
+var num3 = -306;
+var num4 = 92.52;
+
+var str1 = "Hello";
+var str2 = "World";
+var str3 = "Goodbye";
+
+var bool1 = true;
+var bool2 = false;
+
+var undefined1 = ;
+
+console.log(num1, num2, num3);
+console.log(str1, str2, str3);
+console.log(bool1, bool2);
+console.log(underfined1);
