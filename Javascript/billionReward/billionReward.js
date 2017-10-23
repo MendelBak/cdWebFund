f67be765db69d68d6f297c17d4c5e9187b74d389
Mendel <mendelbak@gmail.com>
2017-10-23 08:28:37 -0500

    Finished billionReward algo. Working well w/ all extra credit functionalities. Uploading to learning platform

---
+++ Javascript/billionReward/billionReward.js
@@ -0,0 +1,26 @@
+var reward = 0.01;
+var days = 30;
+
+for (var i = 1; i < days; i++) {
+    reward *= 2; 
+    console.log("$" + reward); }
+
+    console.log("$" + reward);
+
+reward = 0.01;
+
+for (var i = 0; reward <= 10000; i++) {
+    reward *= 2; }
+    console.log("Days to $10,000: " + i, "days.", "Total is $" + reward);
+
+reward = 0.01;
+
+for (var i = 0; reward <= 1000000000; i++) {
+    reward *= 2; }
+    console.log("Days to $1,000,000,000: " + i, "days.", "Total is $" + reward);
+
+var infiniteMoney = 0.01;
+
+    for (var i = 1; i < 1000E+2; i++) {
+        infiniteMoney *= 2; }
+        console.log("$" + infiniteMoney)
