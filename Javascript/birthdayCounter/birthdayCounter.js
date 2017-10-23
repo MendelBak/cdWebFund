8b13ee7943cefba7cade61ee6ce7b89841b46f63
Mendel <mendelbak@gmail.com>
2017-10-22 19:49:06 -0500

    birthdayCounter algo complete. Uploading to learning platform.

---
+++ Javascript/birthdayCounter/birthdayCounter.js
@@ -0,0 +1,25 @@
+var daysUntilMyBirthday = 60;
+
+while (daysUntilMyBirthday > 30) {
+    console.log(daysUntilMyBirthday)
+    console.log("Boo hoo. Nobody likes me!");
+    daysUntilMyBirthday--;
+}
+
+while (daysUntilMyBirthday > 5) {
+    console.log(daysUntilMyBirthday)
+    console.log("My birthday is coming up. I'm so ready to be disappointed by my friends. I'm going to go take pictures of my cat/dog and eat fried food.");
+    daysUntilMyBirthday--;
+}
+
+while (daysUntilMyBirthday > 0) {
+    console.log(daysUntilMyBirthday)
+    console.log("Here it comes! I'd better hide since I hate being the center of attention.");
+    daysUntilMyBirthday--;
+}
+
+while (daysUntilMyBirthday === 0) {
+    console.log(daysUntilMyBirthday)
+    console.log("Hooray! It's my birthday! That wasn't so bad! Let me go do a nice thing for someone else instead of taking all the compliments for myself all day long.");
+    daysUntilMyBirthday--;
+}
